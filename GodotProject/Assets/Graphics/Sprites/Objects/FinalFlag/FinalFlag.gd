c573ee221ab35d070e429da3ecf7a88838877da2
MohamedHaitamKsiks <simpleteam2001@gmail.com>
2020-03-14 09:19:31 +0100

    14/03/2020 --Add first music

---
+++ GodotProject/Assets/Graphics/Sprites/Objects/FinalFlag/FinalFlag.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+signal raise_flag(mario)
+var mario
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_FinalFlag_raise_flag(mario):
+	$AnimationPlayer.play("raise_flag")
+	get_parent().get_node("Music").stop()
+	self.mario = mario 
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "raise_flag" :
+		mario.emit_signal("finish_level")
+		
---
+++ GodotProject/Assets/Graphics/Sprites/Objects/UI/UserInterface.gd
@@ -0,0 +1,39 @@
+extends CanvasLayer
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var blur = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("start")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	draw_coins_counter()
+	draw_health()
+	draw_blur()
+
+func draw_blur():
+	$Blur/BlurRect.get_material().set_shader_param("lod",blur)
+
+
+func draw_coins_counter():
+	var coins = Scores.coins
+	var res = ""
+	var digit = 3
+	
+	for k in range(digit):
+		res += str(int( coins / pow(10,digit - k - 1) ))
+		coins = coins % int(pow(10,digit - k -1 ))
+	$CoinCounter/Label.text = res
+	$CoinCounter/Label2.text = res
+	
+func draw_health():
+	var health = Scores.health
+	$Health/AnimatedSprite.frame = 3 - health
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_parent().get_node("Music").play()
